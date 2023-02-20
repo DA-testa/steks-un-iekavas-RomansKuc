0449833322ee1c1ebb76fda4a4e99e624e408769
RomansKuc <49512692+RomansKuc@users.noreply.github.com>
2023-02-20 03:18:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# python3
+# romans kucerenko 211RDB275 4.grupa
 
 from collections import namedtuple
 
@@ -14,17 +14,19 @@ def find_mismatch(text):
     for i, next in enumerate(text):
         if next in "([{":
             # Process opening bracket, write your code here
+            print("Success")
             pass
 
         if next in ")]}":
             # Process closing bracket, write your code here
+            print("Success")
             pass
 
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
-    # Printing answer, write your code here
+    print(i)
 
 
 if __name__ == "__main__":
