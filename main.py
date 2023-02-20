fa4d986b1634eb973df5b9520c87b67eaea08d1a
RomansKuc <49512692+RomansKuc@users.noreply.github.com>
2023-02-20 04:28:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ def find_mismatch(text):
         if next in ")]}":
             # Process closing bracket, write your code here.
             if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char,next):
-		return str(i+1)
-		opening_brackets_stack.pop()
+				return str(i+1)
+			opening_brackets_stack.pop()
             #print("test2")
             #pass
     if(opening_brackets_stack):
